14ce78589884bcb5630cbd1e79d9eb155114ab25
ebadimobina <ebdimbina@gmail.com>
2025-02-04 14:08:38 +0330

    Revert buggy pull-to-refresh implementation.

--- lib/webview_screen.dart
+++ lib/webview_screen.dart
@@ -13,7 +13,6 @@ class WebViewScreen extends StatefulWidget {
 
 class _WebViewScreenState extends State<WebViewScreen> {
   late final WebViewController webViewController;
-  final ScrollController scrollController = ScrollController();
   var loadingPercentage = 0;
   bool hasInternetConnection = true;
 
@@ -70,50 +69,39 @@ class _WebViewScreenState extends State<WebViewScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: FutureBuilder(
-        future: webViewController.canGoBack(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final canWebViewGoBack = snapshot.data as bool;
-            return PopScope(
-              canPop: !canWebViewGoBack,
-              onPopInvokedWithResult: (didPop, result) {
-                if (!didPop) {
-                  goToPreviousPage(canWebViewGoBack);
-                }
-              },
-              child: Scaffold(
-                body: RefreshIndicator(
-                  // Wrap with RefreshIndicator
-                  onRefresh: refreshWebView,
-                  // Call the refresh function
-                  child: SingleChildScrollView(
-                    controller: scrollController,
-                    physics: AlwaysScrollableScrollPhysics(),
-                    child: SizedBox(
-                      height: MediaQuery.of(context).size.height,
-                      child: hasInternetConnection
-                          ? Stack(
-                              children: [
-                                WebViewWidget(controller: webViewController),
-                                if (loadingPercentage > 0 &&
-                                    loadingPercentage < 100)
-                                  LinearProgressIndicator(
-                                    value: loadingPercentage / 100.0,
-                                  ),
-                              ],
-                            )
-                          : showNetworkErrorScreen(),
-                    ),
-                  ),
+          future: webViewController.canGoBack(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              final canWebViewGoBack = snapshot.data as bool;
+              return PopScope(
+                canPop: !canWebViewGoBack,
+                onPopInvokedWithResult: (didPop, result) {
+                  if (!didPop) {
+                    goToPreviousPage(canWebViewGoBack);
+                  }
+                },
+                child: Scaffold(
+                  body: hasInternetConnection
+                      ? Stack(
+                    children: [
+                      Container(
+                        child:
+                        WebViewWidget(controller: webViewController),
+                      ),
+                      if (loadingPercentage > 0 &&
+                          loadingPercentage < 100)
+                        LinearProgressIndicator(
+                          value: loadingPercentage / 100.0,
+                        ),
+                    ],
+                  )
+                      : showNetworkErrorScreen(),
                 ),
-              ),
-            );
-          } else {
-            return const Center(
-                child: CircularProgressIndicator()); // Loading indicator
-          }
-        },
-      ),
+              );
+            } else {
+              return Container();
+            }
+          }),
     );
   }
 
@@ -153,12 +141,5 @@ class _WebViewScreenState extends State<WebViewScreen> {
         ScaffoldMessenger.of(context).showSnackBar(snackBar);
       }
     }
-  }
-
-  Future<void> refreshWebView() async {
-    setState(() {
-      loadingPercentage = 0; // Reset loading percentage before reload
-    });
-    webViewController.reload();
   }
 }
