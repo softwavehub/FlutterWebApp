4eba24ae55a2e2a740a8a5ea5cd67ac23feb54a7
softwavehub <softwavehub@gmail.com>
2025-07-11 13:41:04 +0530

    changes

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutterwebapp/screen/webview_screen.dart';
 
 // Website to be loaded inside the app
-const websiteUrl = 'https://mobinaebadi.com/';
+const websiteUrl = 'https://silverleafdigha.com/restaurant/';
 
 void main() {
 // Ensure Flutter widgets are prepared
